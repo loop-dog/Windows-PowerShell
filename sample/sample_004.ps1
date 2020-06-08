5b5473c73f7cfb06ae0344c5b6b6d9050a462113
loop-dog <loop_dog2000@hotmail.com>
2020-06-09 00:27:43 +0900

    ハードディスクの一覧を取得
    適宜出力先のドライブは変更してください。

---
+++ sample/sample_004.ps1
@@ -0,0 +1,7 @@
+﻿$OutoputDir = "f:"
+$OutoputDateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+$InitialFileName ="Execute_" + $OutoputDateTime
+
+
+$DiskListName =  $InitialFileName + "_DiskList"
+Get-WmiObject Win32_LogicalDisk  | Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName , VolumeSerialNumber | Where-Object { $_.DriveType -eq "3" } | Export-Csv -Encoding Default $OutoputDir\$DiskListName.csv
