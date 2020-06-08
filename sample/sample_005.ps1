4d8b8235e477af7181bae40320201d5794716f16
loop-dog <loop_dog2000@hotmail.com>
2020-06-09 00:39:10 +0900

    Add files via upload
    ●Windows-PowerShell
    ◎sample
    ・指定外のディスクのファイルの一覧のファイルへの出力（CSV）

---
+++ sample/sample_005.ps1
@@ -0,0 +1,30 @@
+﻿$OutoputDir = "d:"
+$OutoputDateTime = Get-Date -Format "yyyyMMdd_HHmmss"
+$InitialFileName ="Execute_" + $OutoputDateTime
+
+
+$DiskListName =  $InitialFileName + "_DiskList"
+Get-WmiObject Win32_LogicalDisk  | Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName , VolumeSerialNumber | Where-Object { $_.DriveType -eq "3" } | Export-Csv -Encoding Default $OutoputDir\$DiskListName.csv
+
+
+$VolList= Get-WmiObject Win32_LogicalDisk  | Select-Object DeviceID, DriveType, FreeSpace, Size, VolumeName , VolumeSerialNumber | Where-Object { $_.DriveType -eq "3" -and ($_.DeviceID -ne "C:" -and $_.VolumeName -ne "WDC WD20EZRX-00DC0B0" ) }
+
+
+if($VolList -eq $null)
+{
+   Write-Host '出力対象のドライブがありません。';
+}
+else
+{
+   foreach($Vol in $VolList){
+       $TargetDrive = $Vol.DeviceID
+       $TargetVolumeName = $Vol.VolumeName
+       $FileListNames = $InitialFileName + "_" + $TargetVolumeName
+
+       Write-Output $TargetDrive
+       Write-Output $TargetVolumeName
+       Write-Output $FileListNames
+
+       Get-ChildItem $TargetDrive -Recurse | Select-Object Name, FullName, Length, LastWriteTime | Export-Csv -Encoding Default $OutoputDir\$FileListNames.csv
+   }
+}
